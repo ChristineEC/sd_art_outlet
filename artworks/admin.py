78c79b947415257340f2e57a6a2f9197e49a136e
ChristineEC <carterchristinee@gmail.com>
2025-03-08 18:55:26 +0100

    Create artworks app and Artwork Medium and Artist models

---
+++ artworks/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Medium, Artist, Artwork
+
+
+admin.site.register(Medium)
+admin.site.register(Artist)
+admin.site.register(Artwork)
---
+++ artworks/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Medium(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=254)
+    bio = models.TextField(null=True, blank=True)
+    selfie_url = models.URLField(max_length=1024, null=True, blank=True)
+    selfie = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Artwork(models.Model):
+    title = models.CharField(max_length=254)
+    medium = models.ForeignKey('Medium', null=True, blank=True,
+                               on_delete=models.SET_NULL)
+    artist = models.ForeignKey('Artist', null=True, blank=True,
+                               on_delete=models.SET_NULL)
+    dimensions = models.CharField(max_length=254)
+    note = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
