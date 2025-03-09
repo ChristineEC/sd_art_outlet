d01421d8a09fef5681d2e0f38837190b43941e15
ChristineEC <carterchristinee@gmail.com>
2025-03-09 17:13:32 +0100

    Add artwork to database and image credits to README

--- artworks/admin.py
+++ artworks/admin.py
@@ -2,19 +2,32 @@ from django.contrib import admin
 from .models import Medium, Artist, Artwork
 
 
-# class ArtworkAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'title',
-#         'artist',
-#         'medium',
-#         'dimensions',
-#         'price',
-#         'image',
-#         'status',
-#         'custom_made',
-#     )
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'artist',
+        'medium',
+        'dimensions',
+        'price',
+        'image',
+        'status',
+        'custom_made',
+    )
 
 
-admin.site.register(Medium)
-admin.site.register(Artist)
-admin.site.register(Artwork)
+class MediumAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'selfie',
+    )
+
+
+admin.site.register(Medium, MediumAdmin)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Artwork, ArtworkAdmin)
