0e6cc68bab5c34a0bafe1a981753db6ee82a9568
ChristineEC <carterchristinee@gmail.com>
2025-03-09 18:01:52 +0100

    Added artwork_for_sale view and gallery template

--- artworks/views.py
+++ artworks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Artwork
 
-# Create your views here.
+
+def artwork_for_sale(request):
+    """ A view to display all artworks for sale
+    (excludes those already sold),
+    including sorting by medium """
+
+    artworks = Artwork.objects.filter(status=1)
+
+    context = {
+        'artworks': artworks,
+    }
+
+    return render(request, 'artworks/gallery.html', context)
