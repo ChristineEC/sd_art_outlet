95a4f8e5bd3e1bd210da4233ea7b5eacbf793825
ChristineEC <carterchristinee@gmail.com>
2025-03-11 16:17:19 +0100

    Fixed bug with MEDIA_URL not working in templates for manually added files

--- sd_art_outlet/settings.py
+++ sd_art_outlet/settings.py
@@ -68,14 +68,15 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth'),
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
+                "django.template.context_processors.media",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
