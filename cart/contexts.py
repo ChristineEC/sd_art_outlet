36fc319adaf785c142e8f27d0382e1610fe6e4ae
ChristineEC <carterchristinee@gmail.com>
2025-03-12 18:26:45 +0100

    Fix naming error in cart context file

--- cart/contexts.py
+++ cart/contexts.py
@@ -5,12 +5,12 @@ def cart_contents(request):
 
     cart_items = []
     total=0
-    product_count = 0
+    artwork_count = 0
 
     context={
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'artwork_count': artwork_count,
     }
 
     return context
