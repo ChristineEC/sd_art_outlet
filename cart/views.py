c9bdc9ce778bba9346458b11a4ddbdd1077f403c
ChristineEC <carterchristinee@gmail.com>
2025-03-12 21:08:23 +0100

    Added add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from artworks.models import Artwork
+
 
 
 def view_cart(request):
     """A view to show the cart contents page"""
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """Add the specified artwork to the shopping cart"""
+
+    artwork = get_object_or_404(Artwork, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    """Check if item is in cart"""
+    if item_id in list(cart.keys()):
+        messages.error(
+            request,
+            f'{artwork.title} by {artwork.artist} is already in your cart!'
+        )
+    else:
+        cart[item_id] = "item"
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
