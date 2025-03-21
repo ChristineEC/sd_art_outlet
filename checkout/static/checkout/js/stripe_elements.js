eab0abc0b73c7691d1526b25f3ac1b44eced833f
ChristineEC <carterchristinee@gmail.com>
2025-03-21 13:18:03 +0100

    Bug: loading overlay not showing

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -46,7 +46,9 @@ var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(ev) {
   ev.preventDefault();
   card.update({'disabled': true});
-  $('#submit-button').attr('disabled', true); 
+  $('#submit-button').attr('disabled', true);
+  $('#payment-form').fadeToggle(500);
+  $('#loading-overlay').fadeToggle(500);
   stripe.confirmCardPayment(clientSecret, {
     payment_method: {
       card: card
@@ -61,8 +63,10 @@ form.addEventListener('submit', function(ev) {
         </span>
         <span>${result.error.message}</span>`;
         $(errorDiv).html(html);
+        $('#payment-form').fadeToggle(500);
+        $('#loading-overlay').fadeToggle(500);
         card.update({'disabled': false});
-        $('#submit-button').attr('disabled', false); 
+        $('#submit-button').attr('disabled', false);
     } else {
       if (result.paymentIntent.status === 'succeeded') {
         form.submit();
