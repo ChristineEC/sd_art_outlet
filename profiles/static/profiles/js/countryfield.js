75729d3bfe4fc84c12a9b947fa93e5d838ee445c
ChristineEC <carterchristinee@gmail.com>
2025-03-24 22:32:24 +0100

    Style order history on profile and checkout success page; style country selector

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,11 +1,11 @@
 let countrySelected = $('#id_default_country').val();
 if(!countrySelected) {
-    $('#id_default_country').css('color', '#808080');
+    $('#id_default_country').css('color', '#d3d3d3');
 };
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
     if(!countrySelected) {
-        $(this).css('color', '#808080');
+        $(this).css('color', '#d3d3d3');
     }
     else {
         $(this).css('color', '#000');
