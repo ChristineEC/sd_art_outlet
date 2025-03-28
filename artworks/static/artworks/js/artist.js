f725f1b9f24cf156865467727619210b2ac39e8e
ChristineEC <carterchristinee@gmail.com>
2025-03-28 02:55:42 +0100

    Fix path for cancel button to return artist to their page

--- artworks/static/artworks/js/artist.js
+++ artworks/static/artworks/js/artist.js
@@ -1,30 +1,15 @@
-/* Gets the artist id from url to 
-allows artists to add artwork for themselves as artist
-from their own page */
-
-// const backToArtistPage = document.getElementById("back-to-artist-page")
-// const artistCancelAddLinks = document.getElementsByClassName("back-to-artist-page")
+/* Allows redirect back to artist's own page from
+the artist's 'add_an_artwork' page upon
+hitting the cancel link (button-styled) */
 
 var full_url = window.location.pathname;
 var url_array = full_url.split('/');
 var id = url_array[url_array.length-2];
-console.log(id) //This is working
-
 
 function updateHref() {
     const link = document.getElementById("back-to-artist-page");
     let artistId = id;
-    link.href = `artist_page'/${artistId}/`;  // this is all working fine
+    link.href = `/artworks/artist/${artistId}/`;
 }
 
 document.addEventListener("DOMContentLoaded", updateHref);
-
-//     for (let link of artistCancelAddLinks) {
-
-//     link.addEventListener("click", (e) => {
-//         let artistId = id;
-//         console.log(artistId)
-//         artistCancelAddLinks.href = `artist_page/${artistId}`;
-//     });
-// }
-// }
