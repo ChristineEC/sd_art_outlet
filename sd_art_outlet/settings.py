5938aede844d0a3457050c125cdc0235f28e0e9b
ChristineEC <carterchristinee@gmail.com>
2025-03-31 21:07:19 +0200

    Revise settings for AWS in production and add favicon files to image folder

--- sd_art_outlet/settings.py
+++ sd_art_outlet/settings.py
@@ -190,13 +190,15 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET ACCESS_KEY")
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = "sdartoutlet.s3.amazonaws.com"
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+    STATIC_URL = (
+        "https://sdartoutlet.s3.amazonaws.com/static/"
+    )
+    MEDIA_URL = "https://sdartoutlet.s3.amazonaws.com/media/"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
