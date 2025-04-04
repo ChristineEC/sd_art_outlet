b57042ea85557d1c3612b9a9b3953e5f258ba4e1
ChristineEC <carterchristinee@gmail.com>
2025-04-04 12:39:08 +0200

    Enable sending real emails

--- sd_art_outlet/settings.py
+++ sd_art_outlet/settings.py
@@ -117,8 +117,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -129,10 +127,6 @@ SIGNUP_URL = '/accounts/signup/'
 LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = "sd_art_outlet.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {
@@ -215,4 +209,15 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
-DEFAULT_FROM_EMAIL = "sandiegoartoutlet@gmail.com"
+
+if "DEVELOPMENT" in os.environ"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "sandiegoartoutlet@gmail.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
