450239b6f5a674a032dbee502b5fb7ff514420a4
ChristineEC <carterchristinee@gmail.com>
2025-04-12 21:24:12 +0200

    Remove unused image_url fields in models

--- artworks/models.py
+++ artworks/models.py
@@ -25,7 +25,6 @@ class Artist(models.Model):
                                 on_delete=models.SET_NULL)
     mini_bio = models.CharField(max_length=250, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     image_alt = models.CharField(max_length=254, default="Image of the artist")
 
@@ -50,7 +49,6 @@ class Artwork(models.Model):
     )
     dimensions = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     status = models.PositiveSmallIntegerField(choices=STATUS, default=1)
     custom_made = models.BooleanField(default=False)
